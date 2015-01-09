da48350926f6bee4ac99188f23752f94fc248af1
Ben Greear <greearb@candelatech.com>
2015-01-08 16:08:41 -0800

    ath10k: add firmware wmi keep-alive message.
    Only useful for CT firmware.
    
    This sends a wmi message to the firmware every 2 seconds or so.
    Once CT firmware receives one of these messages, it will assert
    if it does not receive more within a 10 second window.
    
    This helps debug and work-around wmi transport hangs.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -666,6 +666,7 @@ struct ath10k_debug {
 
 	unsigned long htt_stats_mask;
 	struct delayed_work htt_stats_dwork;
+	struct delayed_work nop_dwork;
 	struct ath10k_dfs_stats dfs_stats;
 	struct ath_dfs_pool_stats dfs_pool_stats;
 
@@ -682,6 +683,7 @@ struct ath10k_debug {
 	u32 nf_cal_period;
 	void *cal_data;
 	u32 enable_extd_tx_stats;
+	u32 nop_id;
 
 	struct ath10k_dbglog_entry_storage dbglog_entry_data;
 };

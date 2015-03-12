f63065143332574f4b6159aabfa7daf3447edb6f
Ben Greear <greearb@candelatech.com>
2015-03-12 14:41:27 -0700

    ath10k: Allow setting bcast, mcast, mgt fixed tx rates.
    The default behaviour when setting a fixed transmit rate in ath10k
    is to only set the 'ucast' rate.  This does not affect beacons,
    multicast, or broadcast traffic.
    
    In order to allow a back-door way to set these other types of
    traffic as well, provide a debugfs file that can set the
    traffic type.  Then, use can use the normal 'iw' commands to
    set the rate to the desired value.
    
    Upstream firmware *should* support all of this, but at least
    10.1 does NOT have support for the WMI_10X_VDEV_PARAM_MGMT_RATE
    parameter.  I added this to the CT firmware so it works there.
    
    Possibly other upstream firmware have fixed this as well.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -1024,6 +1024,7 @@ struct ath10k {
 	bool ani_enabled;
 	/* protected by conf_mutex */
 	u8 ps_state_enable;
+	u32 set_rate_type; /* override for set-rate behaviour */
 
 	bool nlo_enabled;
 	bool p2p;

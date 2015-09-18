d32924fa2e20a2bb3cfca78221fbc971cf41f070
Ben Greear <greearb@candelatech.com>
2015-09-18 13:02:52 -0700

    ath10k: Don't request OFDM IE when there are no OFDM rates.
    This makes the IE's in the ProbeRequests not advertise rates
    that the vif cannot use.
    
    Also, add some CT-firmware only flags that allow disabling
    HT and VHT ies.
    
    As an aside, at least for CCK and OFDM rates, the mac80211
    stack is already creating IEs for this, so probe requests
    end up with duplicate entries.  This issue is not addressed
    in this patch.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -90,6 +90,9 @@
 /* The magic used by QCA spec */
 #define ATH10K_SMBIOS_BDF_EXT_MAGIC "BDF_"
 
+// TODO-BEN:  Remove this and fix all instances of vif_to_arvif.
+#define ath10k_vif_to_arvif(a) (void*)(a->drv_priv)
+
 struct ath10k;
 
 static inline const char *ath10k_bus_str(enum ath10k_bus bus)

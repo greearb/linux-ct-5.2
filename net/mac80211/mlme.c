4d1cdec67272d917a9073ac5949bcb557eda97ce
Ben Greear <greearb@candelatech.com>
2019-04-18 13:37:25 -0700

    Revert "mac80211: Fix bad formatting of micro-seconds in debugging printout."
    This reverts commit dd5291fe7466e5cecffff867eed8c49915f5fca5.

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -2866,7 +2866,7 @@ static bool ieee80211_mark_sta_auth(struct ieee80211_sub_if_data *sdata,
 	{
 		struct timeval tv;
 		do_gettimeofday(&tv);
-		sdata_info(sdata, "authenticated at: %lu.%06lu\n",
+		sdata_info(sdata, "authenticated at: %lu.%lu\n",
 			   tv.tv_sec, tv.tv_usec);
 	}
 
@@ -3564,7 +3564,7 @@ static void ieee80211_rx_mgmt_assoc_resp(struct ieee80211_sub_if_data *sdata,
 		struct timeval tv;
 		do_gettimeofday(&tv);
 		sdata_info(sdata,
-			   "RX %sssocResp from %pM (capab=0x%x status=%d aid=%d) at: %lu.%06lu\n",
+			   "RX %sssocResp from %pM (capab=0x%x status=%d aid=%d) at: %lu.%lu\n",
 			   reassoc ? "Rea" : "A", mgmt->sa,
 			   capab_info, status_code, (u16)(aid & ~(BIT(15) | BIT(14))),
 			   tv.tv_sec, tv.tv_usec);
@@ -3615,7 +3615,7 @@ static void ieee80211_rx_mgmt_assoc_resp(struct ieee80211_sub_if_data *sdata,
 		{
 			struct timeval tv;
 			do_gettimeofday(&tv);
-			sdata_info(sdata, "associated at: %lu.%06lu\n",
+			sdata_info(sdata, "associated at: %lu.%lu\n",
 				   tv.tv_sec, tv.tv_usec);
 		}
 
@@ -4218,7 +4218,7 @@ static int ieee80211_auth(struct ieee80211_sub_if_data *sdata)
 	{
 		struct timeval tv;
 		do_gettimeofday(&tv);
-		sdata_info(sdata, "send auth to %pM (try %d/%d) at: %lu.%06lu\n",
+		sdata_info(sdata, "send auth to %pM (try %d/%d) at: %lu.%lu\n",
 			   auth_data->bss->bssid, auth_data->tries,
 			   max_auth_tries, tv.tv_sec,
 			   tv.tv_usec);
@@ -5183,7 +5183,7 @@ int ieee80211_mgd_auth(struct ieee80211_sub_if_data *sdata,
 	{
 		struct timeval tv;
 		do_gettimeofday(&tv);
-		sdata_info(sdata, "%pM authenticate with %pM at: %lu.%06lu\n",
+		sdata_info(sdata, "%pM authenticate with %pM at: %lu.%lu\n",
 			   sdata->dev->dev_addr, req->bss->bssid,
 			   tv.tv_sec, tv.tv_usec);
 	}
